6e0f9ebe9245b8cfd56a98f3d3ce4fe05438075e
gentom <gento.m.developer@gmail.com>
2019-03-07 15:26:30 +0900

    init Client

--- shipco.go
+++ shipco.go
@@ -1,6 +1,13 @@
 package shipco
 
+import (
+	"net/http"
+	"net/url"
+)
+
 // Client (Ship&Co API Client)
 type Client struct {
-	Token string `json:"token"`
+	Token  string `json:"token"`
+	URL    *url.URL
+	client *http.Client
 }
