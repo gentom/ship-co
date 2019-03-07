375446e26e9241f202c0159a1a1e16888cfc2fb6
gentom <gento.m.developer@gmail.com>
2019-03-07 15:51:26 +0900

    implement NewClient func

--- shipco.go
+++ shipco.go
@@ -3,6 +3,7 @@ package shipco
 import (
 	"net/http"
 	"net/url"
+	"strings"
 )
 
 // Client (Ship&Co API Client)
@@ -10,4 +11,24 @@ type Client struct {
 	Token  string `json:"token"`
 	URL    *url.URL
 	client *http.Client
+}
+
+// NewClient returns Client struct's instance
+func NewClient(URL, token string) *Client {
+	c := &Client{
+		Token:  token,
+		client: http.DefaultClient,
+	}
+
+	if !strings.HasSuffix(URL, "/") {
+		URL += "/"
+	}
+
+	var err error
+	c.URL, err = url.Parse(URL)
+	if err != nil {
+		panic(err)
+	}
+
+	return c
 }
